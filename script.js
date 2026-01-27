b5097a6e1b43ba9f651eb9eaaa0e79dd2611cb2e
RafaDev BackEnd <rm9589648@gmail.com>
2026-01-27 13:34:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+emailjs.init("78s0f0hGN_NGHAP67");
+
 const btn_submit = document.getElementById("enviar");
 
 btn_submit.addEventListener("click", async function(event){  // async faz esperar a resposta da api para seguir para a proxima linha
@@ -11,27 +13,28 @@ btn_submit.addEventListener("click", async function(event){  // async faz espera
         alert("Todos os campos devem ser preenchidos!");
         return;
     }
+
+    btn_submit.innerText = "Enviando...";
+    btn_submit.disabled = true;
+
+    const templateParams = {
+        nome: nome,
+        email:email,
+        proposta: proposta
+    };
+
     try{                                                   // try e catch se o servidor estiver fora do ar o site não quebra, exibe que o servidor esta fora do ar
-        const resposta = await fetch('https://neomax.onrender.com/api/requisicao', {  // await faz esperar a resposta da api para seguir para a proxima linha
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({nome, email, proposta})
-        });
-
-        if(resposta.ok){                                   // Verifica se os status do http foi um sucesso(200 ou 201)
-            const confirmado = document.getElementById("confirmar_proposta");
-            const confirmar = document.getElementById("form-container");
-
-            confirmado.style.display = "flex";
-            confirmar.style.display = "none";
-
-            console.log("Sucesso: Proposta enviada ao servidor.");
-        }else{
-            alert("Erro no servidor. Tente novamente mais tarde.")
+        const resposta = await emailjs.send('service_4hk253t', 'template_adjg0f9', templateParams);
+        if(resposta.status == 200){
+            document.getElementById("confirmar_proposta").style.display = "flex";
+            document.getElementById("form-container").style.display = "none";
+            console.log("Sucesso: E-mail enviado via EmailJS.");
         }
-        }catch(error){
-            console.error("Erro na requisição: ", error);
-            alert("Falha na conexão. Verifique sua internet.");
-    }
-
+    }  catch(error){
+        console.error("Erro no EmailJS: ", error);
+        alert("Falha ao enviar e-mail. Tente novamente.");
+    }  finally{
+        btn_submit.innerText = "Enviar";
+        btn_submit.disabled = false;
+    } 
 });
